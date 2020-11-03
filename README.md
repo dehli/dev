e1b5070f97ccea8718ed1c906647b9937657b8cd
Christian Paul Dehli <github@deh.li>
2020-11-02 22:56:32 -0500

    Updates to AmazonLinux2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Getting Started
+
+1. `npm install`
+2. `npm run deploy`
+3. Edit inbound rules for created security group:
+  > SSH, Your IP Address
--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-import { App, Stack } from "monocdk";
-import { Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Vpc } from "monocdk/aws-ec2";
+import * as core from "monocdk";
+import * as ec2 from "monocdk/aws-ec2";
 
-const app = new App();
-const stack = new Stack(app, "DehliDev", {
+const app = new core.App();
+const stack = new core.Stack(app, "DehliDev", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
   }
 });
 
-new Instance(stack, "Instance", {
-  instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
-  machineImage: MachineImage.latestAmazonLinux(),
-  vpc: Vpc.fromLookup(stack, "Vpc", { isDefault: true })
+new ec2.Instance(stack, "Instance", {
+  instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+  machineImage: ec2.MachineImage.latestAmazonLinux({
+    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+  }),
+  vpc: ec2.Vpc.fromLookup(stack, "Vpc", { isDefault: true })
 });
